1f3e388b488f0df45201423ee2f0a8e4d4d94187
Daniel <daniel.galperson@gmail.com>
2020-07-27 20:39:01 +0300

    wip

--- city_crawler.py
+++ city_crawler.py
@@ -1,8 +1,10 @@
 import re
-
+import time
 import bs4
 import requests
-import time
+import tqdm
+
+proxies = {"http":"http://192.117.146.110"}
 
 def get_items_for_city(page:int=1, city_id:int=7900, verbose:bool=False):
     """
@@ -11,7 +13,7 @@ def get_items_for_city(page:int=1, city_id:int=7900, verbose:bool=False):
     :param verbose: choose whether to print the found ids and the match number
     :return: list of all the item ids on the specific page - for example - ['wokcob', 'keqxe0', '06mp98', '9njbjv', ... ]
     """
-    res = requests.get(f'https://www.yad2.co.il/realestate/forsale?city={city_id}&page={page}')
+    res = requests.get(f'https://www.yad2.co.il/realestate/forsale?city={city_id}&page={page}', proxies=proxies)
     soup = bs4.BeautifulSoup(res.text, 'lxml')
     blocks = soup.select('.feeditem.table')
     pattern = r'item-id=\"......\"'
@@ -20,7 +22,7 @@ def get_items_for_city(page:int=1, city_id:int=7900, verbose:bool=False):
     for matchNum, match in enumerate(matches, start=1):
         if (verbose):
             print("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum=matchNum, start=match.start(),
-                                                                            end=match.end(), match=match.group()))
+                                                                                end=match.end(), match=match.group()))
         results.append(match.group()[9:-1])
 
     return results
@@ -34,3 +36,34 @@ def get_all_item_ids(city_id:int=7900, limit:int=100):
 
     print(f"found {id_list} ids")
     return id_list
+
+def get_item_by_id(item_id:str):
+    api_link = f'https://www.yad2.co.il/api/item/{item_id}'
+    response = requests.get(api_link, proxies=proxies)
+    result = response.json()
+    print(result)
+    return result
+
+# TODO: Optimize method for multiple async calls
+def get_item_by_list_ids (item_list:list, sleep_interval_sec:int=1):
+    results = {}
+    for i in tqdm.tqdm(item_list):
+        try:
+            results[i] = get_item_by_id(i)
+            time.sleep(sleep_interval_sec)
+        except Exception as e:
+            print(f"Unable to get item: {i}")
+    return results
+
+# city_ids = get_items_for_city(67,7900,True)
+city_ids = get_all_item_ids(7900,10)
+print(city_ids)
+results = (get_item_by_list_ids(city_ids))
+print(results)
+
+import pickle
+# with open("data.obj", 'rb') as f:
+    # a = pickle.load(f)
+    # print(a)
+with open("data2.obj", 'wb') as f:
+    pickle.dump(results,f)
